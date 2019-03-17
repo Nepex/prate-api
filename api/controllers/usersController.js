9e1727cc67779191e9b86ece175573e5b12a98b1
nepex <mattb103190@gmail.com>
2019-03-17 14:11:38 -0500

    user posting data

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,67 +1,73 @@
 const server = require('../../connection.js');
+const bcrypt = require('bcrypt');
+const uuidv4 = require('uuid/v4');
 
 const getUsers = (request, response) => {
-    server.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+  server.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  server.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const createUser = (request, response) => {
+  const { name, email, password } = request.body
+
+  bcrypt.hash(password, 10, function (err, hash) {
+    const id = uuidv4();
+
+    server.query('INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4)', [id, name, email, hash], (error, result) => {
       if (error) {
         throw error
       }
-      response.status(200).json(results.rows)
+      response.status(201).send({})
     })
-  }
-  
-  const getUserById = (request, response) => {
-    const id = parseInt(request.params.id)
-  
-    server.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+  });
+}
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email } = request.body
+
+  server.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
       if (error) {
         throw error
       }
-      response.status(200).json(results.rows)
-    })
-  }
-  
-  const createUser = (request, response) => {
-    const { name, email } = request.body
-  
-    server.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(201).send(`User added with ID: ${result.insertId}`)
-    })
-  }
-  
-  const updateUser = (request, response) => {
-    const id = parseInt(request.params.id)
-    const { name, email } = request.body
-  
-    server.query(
-      'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-      [name, email, id],
-      (error, results) => {
-        if (error) {
-          throw error
-        }
-        response.status(200).send(`User modified with ID: ${id}`)
-      }
-    )
-  }
-  
-  const deleteUser = (request, response) => {
-    const id = parseInt(request.params.id)
-  
-    server.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).send(`User deleted with ID: ${id}`)
-    })
-  }
-  
-  module.exports = {
-    getUsers,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser,
-  }
+      response.status(200).send(`User modified with ID: ${id}`)
+    }
+  )
+}
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  server.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`User deleted with ID: ${id}`)
+  })
+}
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+}
